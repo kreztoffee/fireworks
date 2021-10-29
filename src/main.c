28d3754b5ee212f3d3f6b28375dfc0cc88944a18
chris <bigc123@hotmail.co.uk>
2021-10-29 21:36:02 +0100

    plug stuff into main

--- src/main.c
+++ src/main.c
@@ -7,6 +7,10 @@
 #include "window.h"
 #include "particle.h"
 #include "time_fw.h"
+#include "shader.h"
+
+#include "renderer.h"
+
 #include <stdlib.h>
 
 #define WIDTH 640
@@ -15,27 +19,34 @@
 int main() {
 	init_glfw();
 	window_t* window = create_window(WIDTH, HEIGHT, "fireworks");
-	struct time_fw* time_fw = (struct time_fw*)malloc(sizeof(struct time_fw));
+	struct time_fw* time_fw = init_time_fw();
 	init_glew();
 
 	init_keyboard_handler(window);
 	init_mouse_handler(window);
+	init_particle_system();
 
-	init_time_fw(time_fw);
+	GLuint shader_stages[2];
+	shader_stages[0] = load_shader("../res/basic_shader_2d.vert");
+	shader_stages[1] = load_shader("../res/basic_shader_2d.frag");
+	GLuint basic_prog_id = create_program(&shader_stages[0], 2);
+
+	prepare_renderer(basic_prog_id, window);
 
 	poll_events();
 	while (!should_window_close(window)) {
 		step_time_fw(time_fw);
 
-		handle_input(window);
-
-		/* render stuff */
-		const GLfloat clear_color[] = {0.2f, 0.2f, 0.5f, 1.0f};
+		const GLfloat clear_color[] = {0.01f, 0.01f, 0.1f, 1.0f};
 		glClearBufferfv(GL_COLOR, 0, clear_color);
-		render_particles();
+		while (time_fw->lag >= MS_PER_UPDATE) {
+			time_fw->lag -= MS_PER_UPDATE;
+			update_particles(MS_PER_UPDATE / 1000.0f);
+		}
+		render_particles(basic_prog_id);
 
 		swap_buffers(window);
-		/*print_time_fw(time_fw);*/
+		print_time_fw(time_fw);
 		poll_events();
 	}
 
